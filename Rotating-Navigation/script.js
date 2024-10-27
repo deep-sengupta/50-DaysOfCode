bb7e4bbf4bb6575b2344985c946e04ed1a2d8c2f
Deep SenGupta <myselfdeepsengupta@gmail.com>
2024-10-27 21:19:59 +0530

    Add Files

---
+++ Rotating-Navigation/script.js
@@ -0,0 +1,4 @@
+const circle = document.getElementById('toggle');
+const container = document.querySelector('.container');
+
+circle.addEventListener('click', () => container.classList.toggle('show-nav'));
